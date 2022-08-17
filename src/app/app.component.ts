a23356c428aecb451660dfcbb04f6ae57425c145
Simon Cheam <scheamrevation.com@Simons-MacBook-Pro-2.local>
2022-08-17 17:18:02 -0400

    added index prop in ngFor, pushing datetime into log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,8 @@ export class AppComponent {
 
   onToggleDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1)
+    //this.log.push(this.log.length + 1)
+    this.log.push(new Date())
 
   }
 
